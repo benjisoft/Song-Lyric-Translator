44e279c6e72d5590197c1c5c979ce2174482cb70
benmemons <ben@emmons.biz>
2018-06-23 13:16:00 +0100

    Added lyrics api stuff

--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -1,6 +1,6 @@
 function search() {
   query = document.getElementById("search_query").value.split(" by ")
   url = "http://lyric-api.herokuapp.com/api/find/" + query[1] + "/" + query[0]
-  return $.getJSON(url, function(result){return result})
-
+  lyrics = $.getJSON(url, function(result){result})["responseJSON"]
+  return lyrics
 }
