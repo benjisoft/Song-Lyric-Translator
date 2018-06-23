a1f4208b44695fe5cb10cba88d4b180d9c29cbb5
Benji <benji@benjisoft.org.uk>
2018-06-23 12:02:06 +0100

    Fix

--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -1,11 +1,3 @@
-function onClientLoad() {
-  gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-  gapi.client.setApiKey('AIzaSyApfsVygZaxH3C8Z8a_WJoqzyvDy-7OhoU');
-}
-
 function search() {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", "https://www.googleapis.com/youtube/v3/search?q=" + document.getElementById("search_query").value + "&maxResults=1&part=snippet&key=AIzaSyApfsVygZaxH3C8Z8a_WJoqzyvDy-7OhoU", false ); // false for synchronous request
