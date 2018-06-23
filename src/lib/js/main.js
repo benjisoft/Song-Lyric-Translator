07a6143e7fd79b5ba351b97f2ef14033e8d3efc5
benmemons <ben@emmons.biz>
2018-06-23 13:10:04 +0100

    Got lyrics API

--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -1,7 +1,6 @@
 function search() {
-  var xmlHttp = new XMLHttpRequest();
   query = document.getElementById("search_query").value.split(" by ")
-  xmlHttp.open( "GET", "http://lyric-api.herokuapp.com/api/find/" + query[1] + "/" + query[0], false ); // false for synchronous request
-  xmlHttp.send( null );
-  return JSON.parse(xmlHttp.responseText)
+  url = "http://lyric-api.herokuapp.com/api/find/" + query[1] + "/" + query[0]
+  return $.getJSON(url, function(result){return result})
+
 }
