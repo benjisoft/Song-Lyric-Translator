d96c83917b1090a0aa0b53f56e01b5f172030d85
Benji <benji@benjisoft.org.uk>
2018-06-23 15:20:49 +0100

    Disappearing buttons

--- src/lib/js/click.js
+++ src/lib/js/click.js
@@ -2,6 +2,7 @@ var input = document.getElementById("search_query");
 
 input.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
+        document.getElementById("buttons").style.display = "block";
         search()
     }
 });
