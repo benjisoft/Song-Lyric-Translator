9b803cbb31dc53947bff2d9a0552d484f25bc8f6
Benji <benji@benjisoft.org.uk>
2018-06-23 11:59:13 +0100

    Fixed

--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -6,34 +6,9 @@ function onYouTubeApiLoad() {
   gapi.client.setApiKey('AIzaSyApfsVygZaxH3C8Z8a_WJoqzyvDy-7OhoU');
 }
 
-// Called when the search button is clicked in the html code
 function search() {
-  var query = document.getElementById('search_query').value;
-  // Use the JavaScript client library to create a search.list() API call.
-  var request = gapi.client.youtube.search.list({
-      part: 'snippet',
-      q:query
-  });
-  // Send the request to the API server, call the onSearchResponse function when the data is returned
-  request.execute(onSearchResponse);
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", "https://www.googleapis.com/youtube/v3/search?q=" + document.getElementById("search_query").value + "&maxResults=1&part=snippet&key=AIzaSyApfsVygZaxH3C8Z8a_WJoqzyvDy-7OhoU", false ); // false for synchronous request
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
 }
-// Triggered by this line: request.execute(onSearchResponse);
-function onSearchResponse(response) {
-  var responseString = JSON.stringify(response, '', 2);
-  document.getElementById('response').innerHTML = responseString;
-}
-
-
-function search() {
-    var q = $('#search_query').val();
-    var request = gapi.client.youtube.search.list({
-      q: q,
-      part: 'snippet'
-    });
-  
-    request.execute(function(response) {
-      var str = JSON.stringify(response.result);
-      $('#response').html('<pre>' + str + '</pre>');
-    });
-  }
-
