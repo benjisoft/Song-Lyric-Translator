8c4f42de82b3bba97e0e2c000a897ef45da3933d
benmemons <ben@emmons.biz>
2018-06-23 14:13:09 +0100

    Finally got lyrics API to work

--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -1,6 +1,21 @@
 function search() {
   query = document.getElementById("search_query").value.split(" by ")
-  url = "http://lyric-api.herokuapp.com/api/find/" + query[1] + "/" + query[0]
-  lyrics = $.getJSON(url, function(result){result})["responseJSON"]
-  return lyrics
-}
+  url = "http://benbot-translator.herokuapp.com/api/find/" + query[1] + "/" + query[0]
+
+  $.ajax({
+    type: "GET",
+    data: {
+        apikey:"a2cb19040a4a1174fc3ccea1a22d622c",
+        q_artist: query[1],
+        q_track: query[0],
+        format:"jsonp",
+        callback:"jsonp_callback"
+    },
+    url: "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
+    dataType: "jsonp",
+    jsonpCallback: 'jsonp_callback',
+    contentType: 'application/json',
+    success: function(data) {
+      console.log(data)
+    }})
+  }
