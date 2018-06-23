3631eb4e5cfe14e7baa567fd04422f6823fb9384
benmemons <ben@emmons.biz>
2018-06-23 11:45:34 +0100

    Still getting youtube api to work

--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -6,34 +6,20 @@ function onYouTubeApiLoad() {
   gapi.client.setApiKey('AIzaSyApfsVygZaxH3C8Z8a_WJoqzyvDy-7OhoU');
 }
 
-// Called when the search button is clicked in the html code
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+
+// Search for a specified string.
 function search() {
-  var query = document.getElementById('search_query').value;
-  // Use the JavaScript client library to create a search.list() API call.
+  var q = $('#search_query').val();
   var request = gapi.client.youtube.search.list({
-      part: 'snippet',
-      q:query
+    q: q,
+    part: 'snippet'
   });
-  // Send the request to the API server, call the onSearchResponse function when the data is returned
-  request.execute(onSearchResponse);
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#response').html('<pre>' + str + '</pre>');
+  });
 }
-// Triggered by this line: request.execute(onSearchResponse);
-function onSearchResponse(response) {
-  var responseString = JSON.stringify(response, '', 2);
-  document.getElementById('response').innerHTML = responseString;
-}
-
-
-function search() {
-    var q = $('#search_query').val();
-    var request = gapi.client.youtube.search.list({
-      q: q,
-      part: 'snippet'
-    });
-  
-    request.execute(function(response) {
-      var str = JSON.stringify(response.result);
-      $('#response').html('<pre>' + str + '</pre>');
-    });
-  }
-
